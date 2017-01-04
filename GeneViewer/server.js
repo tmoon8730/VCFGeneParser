74342a5ced2d61e2de17537d3b6b5600c93f98b4
Tyler Moon <moon.tyler@gmail.com>
2017-01-03 23:35:30 -0500

    Added some more styling and a color scheme

--- GeneViewer/server.js
+++ GeneViewer/server.js
@@ -1,18 +1,13 @@
 var express = require('express');
 var app = express()
-app.set('view engine', 'pug');
 var router = express.Router();
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
-/* Set up the MongoDB Database connection */
 var mongoose = require('mongoose');
 var url = 'mongodb://localhost:27017/gene_database';
 var assert = require('assert');
 
-mongoose.connect(url, function(err){
-	assert.equal(err,null);
-	console.log('connected to mongodb');
-});
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended:true}));
 
 var Schema = mongoose.Schema, ObjectID = Schema.ObjectID;
 
@@ -20,57 +15,41 @@ var Gene = new Schema({
 	gene_id: String,
 	pos: String,
 	chrom: String,
+	ref: String,
+	alt: [String],
 },{collection: 'gene_collection'})
 
 var Gene = mongoose.model('Gene', Gene);
 
+mongoose.connect(url, function(err){
+	assert.equal(err,null);
+	console.log('connected to mongodb');
+});
+
 app.get('/', function(req, res){
-	Gene.find({}, function(err, docs){
-		console.log('Docs = ' + JSON.stringify(docs))
-		res.render('index', {title:'Hey', message: 'Hello there!', geneId: 'n/a', chrom: 'n/a', pos: 'n/a'});
-	});
+	res.render('index', {title:'Hey', message: 'Hello there!',
+	geneId: 'n/a', chrom: 'n/a', pos: 'n/a', ref: 'n/a', alt: ['n/a','n/a']});
 });
 
 app.post('/', function(req, res){
 	console.log(req.body.gene_id);
 	Gene.findOne({gene_id: req.body.gene_id}, function(err, gene){
-		if(err){
-			res.render('index', {title:'Hey', message: "Did not find a Gene for " + req.body.gene_id, geneId: 'n/a', chrom: 'n/a', pos: 'n/a'});
+		if(gene != null){
+			if(err){
+				res.render('index', {title:'Hey', message: "Error finding a Gene for " + req.body.gene_id, geneId: 'n/a', chrom: 'n/a', pos: 'n/a'});
+				console.log(err);
+			}
+			console.log("found id %s", gene.gene_id);
+			res.render('index', {title:'Hey', message: "Found a Gene for " + gene.gene_id,
+			geneId: gene.gene_id, chrom: gene.chrom, pos: gene.pos, ref: gene.ref, alt: gene.alt});
+		}else{
+			res.render('index', {title:'Hey', message: 'Could not find a gene for ' + req.body.gene_id,
+			geneId: 'n/a', chrom: 'n/a', pos: 'n/a', ref: 'n/a', alt: ['n/a','n/a']});
 		}
-		console.log("found id %s", gene.gene_id);
-		res.render('index', {title:'Hey', message: "Found a Gene for " + gene.gene_id, geneId: gene.gene_id, chrom: gene.chrom, pos: gene.pos});
 	})
 });
-/*var MongoClient = require('mongodb').MongoClient;
-
-var ObjectID = require('mongodb').ObjectID;
 
 
-var findGenes = function(db, callback){
-	var cursor = db.collection('gene_collection').find();
-	cursor.each(function(err, doc){
-		assert.equal(err,null);
-		if(doc != null){
-			console.dir(doc);
-		} else {
-			callback();
-		}
-	});
-};
-
-MongoClient.connect(url, function(err, db){
-	assert.equal(null, err);
-	findGenes(db, function() {
-		db.close();
-	});
-});*/
-
-/* Create an HTTP server to handle responses */
-//http.createServer(function(request, response) {
-//	response.writeHead(200, {"Content-Type": "text/plain"});
-//	response.write("Hello World");
-//	response.end();
-//}).listen(8888);
 
 app.listen(8888, function(){
 	// Console will print a message
